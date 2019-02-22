60380ee86f020b7253ed4eb1f3b03f3c894d2164
Ben <maiahee@gmail.com>
2019-02-21 21:09:53 -0800

    Revert "Revert "Merge pull request #44""
    This reverts commit 8027253bf9df091df0b7fcbf389e00d88e3def26.

--- SFTPClient/Client.py
+++ SFTPClient/Client.py
@@ -16,19 +16,17 @@ class SFTP(object):
         self.password = password
         self.private_key_password = private_key_password
         self.local_directory = expanduser('~')
-        self.connection = initiate_connection(hostname, username, password, private_key_password)
         if not exists(DOWNLOADS_DIRECTORY):
             mkdir(DOWNLOADS_DIRECTORY)
+        self.connection = self.initiate_connection()
 
     def is_connected(self):
         """Check the connection (using the listdir() method) to confirm that it's active."""
-        if self.connection.listdir():
-            return True
+        return True if self.connection.listdir() else False
 
     # region Commands Section
-    def ping(self, _args):
-        if self.connection.listdir():
-            return 'pong'
+    def ping(self):
+        return "pong" if self.connection.listdir() else "nothing happened"
 
     def ls(self, args):
         """List directory contents on the remote server"""
@@ -95,55 +93,54 @@ class SFTP(object):
         except Exception:
             pass
 
-
-def initiate_connection(hostname, username, password=None, private_key_password=None):
-    # Connect, checking hostkey or caching on first connect
-    # Based off of this stackoverflow question:
-    #     https://stackoverflow.com/questions/53666106/use-paramiko-autoaddpolicy-with-pysftp
-
-    # configure pysftp CnOpts
-    hostkeys = None
-    cnopts = pysftp.CnOpts()  # loads hostkeys from known_hosts.ssh
-    if cnopts.hostkeys.lookup(hostname) is None:
-        logging.debug('Key for host: ' + hostname + ' was not found in known_hosts')
-        hostkeys = cnopts.hostkeys
-        cnopts.hostkeys = None
-
-    args = {'host': hostname,
-            'username': username,
-            'cnopts': cnopts}
-
-    # Determine what type of authentication to use based on parameters provided
-    ssh_key = expanduser('~') + '/.ssh/id_rsa'
-    if password is not None:
-        logging.debug('Using plaintext authentication')
-        args['password'] = password
-    elif isfile(ssh_key):
-        logging.debug('Got SSH key: ' + ssh_key)
-        # the file at ~/.ssh/id_rsa exists - use it as the (default) private key
-        args['private_key'] = ssh_key
-        if private_key_password is not None:
-            logging.debug('Using public key authentication with DER-encoded private key')
-            args['private_key_pass'] = private_key_password
+    def initiate_connection(self):
+        # Connect, checking hostkey or caching on first connect
+        # Based off of this stackoverflow question:
+        #     https://stackoverflow.com/questions/53666106/use-paramiko-autoaddpolicy-with-pysftp
+    
+        # configure pysftp CnOpts
+        hostkeys = None
+        cnopts = pysftp.CnOpts()  # loads hostkeys from known_hosts.ssh
+        if cnopts.hostkeys.lookup(self.hostname) is None:
+            logging.debug('Key for host: ' + self.hostname + ' was not found in known_hosts')
+            hostkeys = cnopts.hostkeys
+            cnopts.hostkeys = None
+    
+        args = {'host': self.hostname,
+                'username': self.username,
+                'cnopts': cnopts}
+    
+        # Determine what type of authentication to use based on parameters provided
+        ssh_key = expanduser('~') + '/.ssh/id_rsa'
+        if self.password is not None:
+            logging.debug('Using plaintext authentication')
+            args['password'] = self.password
+        elif isfile(ssh_key):
+            logging.debug('Got SSH key: ' + ssh_key)
+            # the file at ~/.ssh/id_rsa exists - use it as the (default) private key
+            args['private_key'] = ssh_key
+            if self.private_key_password is not None:
+                logging.debug('Using public key authentication with DER-encoded private key')
+                args['private_key_pass'] = self.private_key_password
+            else:
+                logging.debug('Using public key authentication with plaintext private key')
         else:
-            logging.debug('Using public key authentication with plaintext private key')
-    else:
-        raise ssh_exception.BadAuthenticationType('No supported authentication methods available',
-                                                  ['password', 'public_key'])
-
-    # connect using the authentication type determined above
-    logging.debug('Connecting using arguments: ' + str(args))
-    try:
-        connection = pysftp.Connection(**args)
-    except paramiko.SSHException as e:
-        logging.critical(e)
-        raise
-
-    # On first connect, Save the new hostkey to known_hosts
-    if hostkeys is not None:
-        logging.debug('Appending new hostkey for ' + hostname + ' to known_hosts, and writing to disk...')
-        hostkeys.add(hostname, connection.remote_server_key.get_name(),
-                     connection.remote_server_key)
-        hostkeys.save(pysftp.helpers.known_hosts())
-
-    return connection
+            raise ssh_exception.BadAuthenticationType('No supported authentication methods available',
+                                                      ['password', 'public_key'])
+    
+        # connect using the authentication type determined above
+        logging.debug('Connecting using arguments: ' + str(args))
+        try:
+            connection = pysftp.Connection(**args)
+        except paramiko.SSHException as e:
+            logging.critical(e)
+            raise
+    
+        # On first connect, Save the new hostkey to known_hosts
+        if hostkeys is not None:
+            logging.debug('Appending new hostkey for ' + self.hostname + ' to known_hosts, and writing to disk...')
+            hostkeys.add(self.hostname, connection.remote_server_key.get_name(),
+                         connection.remote_server_key)
+            hostkeys.save(pysftp.helpers.known_hosts())
+    
+        return connection
